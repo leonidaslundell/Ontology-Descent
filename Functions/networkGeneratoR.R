cea46dda5f4f4eb5bd2f9f5e688ccb685e105602
Leonidas Lundell <qwm753@sund.root.ku.dk>
2021-01-25 15:15:20 +0100

    Function generating the igraph ontogly network to match (reactome and GO)

--- Functions/networkGeneratoR.R
+++ Functions/networkGeneratoR.R
@@ -1,24 +1,47 @@
-networkeR <- function(ont = "MF"){
+#BP is slow, all is painfully slow due to size...
+#is there some kind of smart solution of pruning? Not sure...
+
+#Moreover, this should not be generated every singe time, there should be 
+#some kind of check system where the MD5 of the existing file should be checked?
+
+networkeR <- function(ont = "MF", species = "HSA"){
   library(GO.db)
   library(igraph)
   library(data.table)
   
-  #BP is very slow
-  flatNet <- switch(ont,
-                    MF = as.list(GOMFCHILDREN),
-                    BP = as.list(GOBPCHILDREN),
-                    CC = as.list(GOCCCHILDREN),
-                    all = c(as.list(GOCCCHILDREN),
-                            as.list(GOMFCHILDREN),
-                            as.list(GOBPCHILDREN))) 
+  if(!any(ont %in% c("CC", "BP", "MF","All","Reactome"))){
+    stop("ont needs to be one of: CC, BP, MF, All, Reactome")
+  }
   
-  flatNet <- flatNet[!is.na(flatNet)]
-  flatNet <- reshape2::melt(flatNet)
+  if(ont == "Reactome"){
+    
+    flatNet <- fread("https://reactome.org/download/current/ReactomePathwaysRelation.txt", header = F)
+    flatNet <- flatNet[grep(species, V1)]
+    colnames(flatNet) <- c("from", "to") 
+    flatNet
+    
+  }else{
+    
+    flatNet <- switch(ont,
+                      MF = as.list(GOMFCHILDREN),
+                      BP = as.list(GOBPCHILDREN),
+                      CC = as.list(GOCCCHILDREN),
+                      all = c(as.list(GOCCCHILDREN),
+                              as.list(GOMFCHILDREN),
+                              as.list(GOBPCHILDREN))) 
+    
+    flatNet <- flatNet[!is.na(flatNet)]
+    flatNet <- reshape2::melt(flatNet)
+    
+    flatNet <- as.data.table(flatNet)
+    colnames(flatNet) <- c("from", "to") 
+    
+    flatNet <- flatNet[,c("from", "to"):=.(as.character(from), 
+                                           as.character(to))]
+    
+  }
   
-  flatNet <- as.data.table(flatNet)
-  colnames(flatNet) <- c("from", "to") 
+  graphNet <- graph_from_data_frame(flatNet)
   
-  flatNet <- flatNet[,c("from", "to"):=.(as.character(from), as.character(to))]
-  
-  return(graph_from_data_frame(flatNet))
+  return(graphNet)
 }
