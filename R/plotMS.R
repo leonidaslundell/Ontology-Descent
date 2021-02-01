de221b2c77825c7b8ff541d3f1d78ef5ab3d00c3
Mladen Savikj <mladen.savikj@gmail.com>
2021-02-01 12:42:35 +0100

    Plotting functionsbupdate Plot page layot update

--- R/plotMS.R
+++ R/plotMS.R
@@ -1,37 +1,187 @@
-#' Title
+#' Plot Enrichment by Pathway
 #'
-#' @param xval
-#' @param yval
-#' @param fill
+#' @param ontoID
+#' @param ontoTerm
+#' @param pValue
+#' @param clusterNumber
+#' @param clusterName
+#' @param enrichmentScore optional
+#' @param direction optional
+#' @param plotEnrichment TRUE/FALSE defines x axis - default FALSE
+#' @param interactive TRUE/FALSE plotly or ggplot2 - default FALSE
 #'
 #' @return
-#'
-#' @import ggplot2
+#' @import ggplot2 plotly RColorBrewer ggsci
 #' @export
 #'
 #' @examples
-pathwayGraph <- function(xval, yval, fill){
-    ggplot2::ggplot(data = NULL, aes(xval, yval, fill = fill))+
-    geom_point(pch = 21, size = 5)+
-    theme_bw()+xlab("-log10 P Values")+
-    theme(axis.title.y = element_blank(), legend.title = element_blank(),
-          legend.justification = "top")
+
+pathwayGraph <- function(ontoID, ontoTerm, pValue, clusterNumber, clusterName, enrichmentScore = NULL, direction = NULL,
+                         plotEnrichment = FALSE, interactive = FALSE){
+
+  # Create plot data.frame depending on input
+  plot <- data.frame(ontoID, ontoTerm, pValue, clusterNumber, clusterName)
+
+  if (!is.null(enrichmentScore)){plot$enrichmentScore <- enrichmentScore}
+  if (!is.null(direction)){plot$direction <- direction}
+
+  # Set color palette
+  col <- colorRampPalette(RColorBrewer::brewer.pal(9, "Set1"))((length(unique(plot$clusterName))))
+
+  # Option 1: Plot P Value
+  if (!isTRUE(plotEnrichment)){
+    plot <- plot[order(plot$clusterNumber, -log10(plot$pValue)),]
+    plot$ontoID <- factor(plot$ontoID, levels = unique(plot$ontoID))
+    plot$ontoTerm <- factor(plot$ontoTerm, levels = unique(plot$ontoTerm))
+    plot$clusterName <- factor(plot$clusterName, levels = unique(plot$clusterName))
+
+    xlim <- c(floor(min(-log10(plot$pValue))), ceiling(max(-log10(plot$pValue))))
+
+    # Option 1.1: Plot P Value - No direction
+    if (is.null(direction)){
+      p <- ggplot2::ggplot(plot,
+                           ggplot2::aes(x = -log10(pValue), y = ontoTerm,
+                            fill = clusterName, color = clusterName,
+                            label = ontoID, label2 = ontoTerm, label3 = enrichmentScore))+
+        ggplot2::geom_point(pch = 21, size = 3)+
+        ggplot2::scale_x_continuous("-log10 P Value", limits = xlim, breaks = seq(xlim[1], xlim[2], 1))+
+        ggplot2::scale_fill_manual(values = col, name = "Cluster")+
+        ggplot2::scale_color_manual(values = col, name = "Cluster")+
+        ggplot2::theme(axis.title.y = ggplot2::element_blank())
+
+    # Option 1.2: Plot P Value - With Direction
+    } else if (!is.null(direction)){
+      p <- ggplot2::ggplot(plot,
+                           ggplot2::aes(x = -log10(pValue), y = ontoTerm,
+                            fill = clusterName, color = clusterName, shape = direction,
+                            label = ontoID, label2 = ontoTerm, label3 = enrichmentScore))+
+        ggplot2::geom_point(size = 3)+
+        ggplot2::scale_shape_manual(values = c(25,24), name = "Direction")+
+        ggplot2::scale_fill_manual(values = col, name = "Cluster")+
+        ggplot2::scale_color_manual(values = col, name = "Cluster")+
+        ggplot2::scale_x_continuous("-log10 P Value", limits = xlim, breaks = seq(xlim[1], xlim[2], 1))+
+        ggplot2::theme(axis.title.y = ggplot2::element_blank())
+    }
+  }
+
+  # Option 2: Plot Enrichment Score
+  if (isTRUE(plotEnrichment)){
+    plot <- plot[order(plot$clusterNumber, plot$enrichmentScore),]
+    plot$ontoID <- factor(plot$ontoID, levels = unique(plot$ontoID))
+    plot$ontoTerm <- factor(plot$ontoTerm, levels = unique(plot$ontoTerm))
+    plot$clusterName <- factor(plot$clusterName, levels = unique(plot$clusterName))
+
+    xlim <- c(-ceiling(max(abs(plot$enrichmentScore))),ceiling(max(abs(plot$enrichmentScore))))
+
+    p <- ggplot2::ggplot(plot,
+                         ggplot2::aes(x = enrichmentScore, y = ontoTerm,
+                          fill = clusterName, color = clusterName,
+                          label = ontoID, label2 = ontoTerm, label3 = pValue))+
+      ggplot2::geom_vline(xintercept = 0, lty = "dashed")+
+      ggplot2::geom_point(pch = 21, size = 3)+
+      ggplot2::scale_x_continuous("Enrichment Score", limits = xlim, breaks = seq(xlim[1], xlim[2], 1))+
+      ggplot2::scale_fill_manual(values = col, name = "Cluster")+
+      ggplot2::scale_color_manual(values = col, name = "Cluster")+
+      ggplot2::theme(axis.title.y = ggplot2::element_blank())
+  }
+
+  int <- plotly::ggplotly(p, tooltip = c("ontoID", "ontoTerm", "pValue", "enrichmentScore"))
+
+  if (!isTRUE(interactive)){return(p)} else if (isTRUE(interactive)){return(int)}
 }
 
-#' Title
+#' Plot Enrichment by Cluster
 #'
-#' @param xval
-#' @param yval
+#' @param ontoID
+#' @param ontoTerm
+#' @param pValue
+#' @param clusterNumber
+#' @param clusterName
+#' @param enrichmentScore optional
+#' @param direction optional
+#' @param plotEnrichment TRUE/FALSE defines x axis - default FALSE
+#' @param interactive TRUE/FALSE plotly or ggplot2 - default FALSE
 #'
 #' @return
 #'
-#' @import ggplot2
+#' @import ggplot2 plotly RColorBrewer ggsci
 #' @export
 #'
 #' @examples
-clusterGraph <- function(xval, yval){
-  ggplot2::ggplot(data = NULL, aes(xval, yval, fill = yval))+
-    geom_point(pch = 21, size = 5)+
-    theme_bw()+xlab("-log10 P Values")+
-    theme(axis.title.y = element_blank(), legend.position = "none")
+clusterGraph <- function(ontoID, ontoTerm, pValue, clusterNumber, clusterName, enrichmentScore = NULL, direction = NULL,
+                         plotEnrichment = FALSE, interactive = FALSE){
+
+  # Create plot data.frame depending on input
+  plot <- data.frame(ontoID, ontoTerm, pValue, clusterNumber, clusterName)
+
+  if (!is.null(enrichmentScore)){plot$enrichmentScore <- enrichmentScore}
+  if (!is.null(direction)){plot$direction <- direction}
+
+  # Set color palette
+  col <- colorRampPalette(RColorBrewer::brewer.pal(9, "Set1"))((length(unique(plot$clusterName))))
+
+  # Option 1: Plot P Value
+  if (!isTRUE(plotEnrichment)){
+    plot <- plot[order(plot$clusterNumber, -log10(plot$pValue)),]
+    plot$ontoID <- factor(plot$ontoID, levels = unique(plot$ontoID))
+    plot$ontoTerm <- factor(plot$ontoTerm, levels = unique(plot$ontoTerm))
+    plot$clusterName <- factor(plot$clusterName, levels = unique(plot$clusterName))
+
+    xlim <- c(floor(min(-log10(plot$pValue))), ceiling(max(-log10(plot$pValue))))
+
+    # Option 1.1: Plot P Value - No direction
+    if (is.null(direction)){
+      p <- ggplot2::ggplot(plot,
+                           ggplot2::aes(x = -log10(pValue), y = clusterName,
+                            fill = clusterName, color = clusterName,
+                            label = ontoID, label2 = ontoTerm, label3 = enrichmentScore))+
+        ggplot2::geom_point(pch = 21, size = 3)+
+        ggplot2::scale_x_continuous("-log10 P Value", limits = xlim, breaks = seq(xlim[1], xlim[2], 1))+
+        ggplot2::scale_fill_manual(values = col, name = "Cluster")+
+        ggplot2::scale_color_manual(values = col, name = "Cluster")+
+        ggplot2::theme(axis.title.y = ggplot2::element_blank())
+
+      # Option 1.2: Plot P Value - With Direction
+    } else if (!is.null(direction)){
+      p <- ggplot2::ggplot(plot,
+                           ggplot2::aes(x = -log10(pValue), y = direction,
+                            fill = clusterName, color = clusterName,
+                            label = ontoID, label2 = ontoTerm, label3 = enrichmentScore))+
+        ggplot2::geom_point(size = 3)+
+        ggplot2::facet_wrap(~clusterName, ncol = 1, strip.position = "left")+
+        ggplot2::scale_shape_manual(values = c(25,24), name = "Direction")+
+        ggplot2::scale_fill_manual(values = col, name = "Cluster")+
+        ggplot2::scale_color_manual(values = col, name = "Cluster")+
+        ggplot2::scale_x_continuous("-log10 P Value", limits = xlim, breaks = seq(xlim[1], xlim[2], 1))+
+        ggplot2::theme(axis.title.y = ggplot2::element_blank(),
+              strip.placement = "outside", strip.text.y.left = ggplot2::element_text(angle = 0),
+              strip.background = ggplot2::element_blank(), panel.spacing.y = ggplot2::unit(0, "line"))
+    }
+  }
+
+  # Option 2: Plot Enrichment Score
+  if (isTRUE(plotEnrichment)){
+    plot <- plot[order(plot$clusterNumber, plot$enrichmentScore),]
+    plot$ontoID <- factor(plot$ontoID, levels = unique(plot$ontoID))
+    plot$ontoTerm <- factor(plot$ontoTerm, levels = unique(plot$ontoTerm))
+    plot$clusterName <- factor(plot$clusterName, levels = unique(plot$clusterName))
+
+    xlim <- c(-ceiling(max(abs(plot$enrichmentScore))),ceiling(max(abs(plot$enrichmentScore))))
+
+    p <- ggplot2::ggplot(plot,
+                         ggplot2::aes(x = enrichmentScore, y = clusterName,
+                          fill = clusterName, color = clusterName,
+                          label = ontoID, label2 = ontoTerm, label3 = pValue))+
+      ggplot2::geom_vline(xintercept = 0, lty = "dashed")+
+      ggplot2::geom_point(pch = 21, size = 3)+
+      ggplot2::scale_x_continuous("Enrichment Score", limits = xlim, breaks = seq(xlim[1], xlim[2], 1))+
+      ggplot2::scale_fill_manual(values = col, name = "Cluster")+
+      ggplot2::scale_color_manual(values = col, name = "Cluster")+
+      ggplot2::theme(axis.title.y = ggplot2::element_blank())
+  }
+
+  int <- plotly::ggplotly(p, tooltip = c("ontoID", "ontoTerm", "pValue", "enrichmentScore"))
+
+  if (!isTRUE(interactive)){return(p)} else if (isTRUE(interactive)){return(int)}
 }
+
--- R/plotting_page.R
+++ R/plotting_page.R
@@ -3,37 +3,35 @@
 #' @param id unique id for this module
 #'
 #' @import shiny
+#' @import ggplot2 plotly RColorBrewer
 #' @export
 plotting_page_ui <- function(id)
 {
   ns <- shiny::NS(id)
-  fluidPage( # flexible layout function
 
-    # Title
-    titlePanel("Descent Plot"),
+  fluidPage(
+    titlePanel("Ontology Descent: Plotting Enrichment"),
 
-    sidebarLayout(  # standard inputs on sidebar, outputs in main area layout
-      sidebarPanel( # sidebar configuration
-        selectInput(inputId = ns("plotType"),
-                    label = "Plot Type:",
-                    choices = c("By Cluster" = "clust", "By Pathway" = "path"),
-                    multiple = FALSE),
+    fluidRow(
+      column(width = 4,
+             wellPanel(
+               radioButtons(inputId = ns("plotType"), label = "Plot Type",
+                            choices = c("By Pathway" = "pth", "By Cluster" = "clust"),
+                            selected = "clust"),
 
-        actionButton(inputId = ns("actPlot"), "Plot"),
-        numericInput(inputId = ns("ht"), label = "Height (cm)", value = 5.75),
-        numericInput(inputId = ns("wd"), label = "Width (cm)", value = 8.5),
-        selectInput(inputId = ns("fileType"), label = "File Type:",
-                    choices = c("eps", "ps", "tex", "pdf", "jpeg", "tiff", "png", "bmp", "svg", "wmf"),
-                    multiple = FALSE, selected = "tiff"),
-        downloadButton(ns("downPlot"), "Download")
+               radioButtons(inputId = ns("axisType"), label = "Axis Type",
+                            choices = c("P Value" = "p", "Enrichment Score" = "e"),
+                            selected = "p"),
+
+               actionButton(inputId = ns("actPlot"), label = "Show plot")
+             )
       ),
 
-
-      # Show a plot of the generated distribution
-      mainPanel( # main output configuration
-        plotOutput(ns("plot"))
+      column(width = 8,
+             plotOutput(outputId = ns("plotOut")),
+             textOutput(outputId = ns("pathWarning"))
+             )
       )
-    )
   )
 }
 
@@ -50,23 +48,47 @@ plotting_page <- function(input, output, session, descent_data)
 {
   # reactive value (ggplot)
   # render plot (based on reactive value)
-
   observeEvent(input$actPlot, {
-    if (input$plotType == "path"){
-      output$plot <- renderPlot(pathwayGraph(-log10(descent_data$clusterData$pValue),
-                                                                         descent_data$clusterData$ontoTerm,
-                                                                         descent_data$clusterData$clusterName))
+    aT <- switch (input$axisType,
+      "p" = FALSE,
+      "e" = TRUE
+      )
+
+    if (input$plotType == "pth"){
+      dat <- reactive({example_data[1:50,-c(3,5)]})
+
+      output$plotOut <- renderPlot({pathwayGraph(ontoID = dat()$ontoID,
+                                                 ontoTerm = dat()$ontoTerm,
+                                                 pValue = dat()$pValue,
+                                                 clusterNumber = dat()$clusterNumber,
+                                                 clusterName = dat()$clusterName,
+                                                 enrichmentScore = dat()$enrichmentScore,
+                                                 direction = dat()$direction,
+                                                 plotEnrichment = aT,
+                                                 interactive = FALSE)})
+
+      len <- reactive({nrow(example_data)})
+
+      if (len() > 50){
+        output$pathWarning <- renderText("Warning: Plot by pathway supports up to 50 separate ontoIDs.
+                                         The output has been cut to top 50 ontoIDs.
+                                         To plot >50 ontoIDs silmultaneously, please use plot by cluster.")
+      }
+
     } else if (input$plotType == "clust"){
-      output$plot <- renderPlot(clusterGraph(-log10(descent_data$clusterData$pValue),
-                                             descent_data$clusterData$clusterName))
-    }
-    })
+      dat <- reactive({example_data[,-c(3,5)]})
 
-  output$downloadData <- downloadHandler(
-    filename = function(){paste("plot", input$fileType, sep = ".")},
-    content = function(file){
-      ggplot2::ggsave(output$plot, filename)
-    }
-  )
+      output$plotOut <- renderPlot({clusterGraph(ontoID = dat()$ontoID,
+                                                 ontoTerm = dat()$ontoTerm,
+                                                 pValue = dat()$pValue,
+                                                 clusterNumber = dat()$clusterNumber,
+                                                 clusterName = dat()$clusterName,
+                                                 enrichmentScore = dat()$enrichmentScore,
+                                                 direction = dat()$direction,
+                                                 plotEnrichment = aT,
+                                                 interactive = FALSE)})
 
+      output$pathWarning <- renderText("")
+    }
+  })
 }
