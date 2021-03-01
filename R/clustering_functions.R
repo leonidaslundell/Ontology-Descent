59a5805d03db3ae56666ddeed8c6b0fdeedbcee7
Leonidas Lundell <leonidas.lundell@gmail.com>
2021-03-01 17:02:23 +0100

    moved networkeR into clustering_fucntions

--- R/clustering_functions.R
+++ R/clustering_functions.R
@@ -65,3 +65,46 @@ clustereR <- function(ontoNet, ontoNames, ontoLength, target){
 
 }
 
+
+networkeR <- function(ont = "MF", species = "HSA"){
+  library(GO.db)
+  library(igraph)
+  library(data.table)
+
+  if(!any(ont %in% c("CC", "BP", "MF","All","Reactome"))){
+    stop("ont needs to be one of: CC, BP, MF, All, Reactome")
+  }
+
+  if(ont == "Reactome"){
+
+    flatNet <- fread("https://reactome.org/download/current/ReactomePathwaysRelation.txt", header = F)
+    flatNet <- flatNet[grep(species, V1)]
+    colnames(flatNet) <- c("from", "to")
+    flatNet
+
+  }else{
+
+    flatNet <- switch(ont,
+                      MF = as.list(GOMFCHILDREN),
+                      BP = as.list(GOBPCHILDREN),
+                      CC = as.list(GOCCCHILDREN),
+                      all = c(as.list(GOCCCHILDREN),
+                              as.list(GOMFCHILDREN),
+                              as.list(GOBPCHILDREN)))
+
+    flatNet <- flatNet[!is.na(flatNet)]
+    flatNet <- reshape2::melt(flatNet)
+
+    flatNet <- as.data.table(flatNet)
+    colnames(flatNet) <- c("from", "to")
+
+    flatNet <- flatNet[,c("from", "to"):=.(as.character(from),
+                                           as.character(to))]
+
+  }
+
+  graphNet <- graph_from_data_frame(flatNet)
+
+  return(graphNet)
+}
+
