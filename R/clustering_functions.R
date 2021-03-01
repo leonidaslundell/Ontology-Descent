d0bad3d87b3dbc82ba823d9c4e9592b1b5ad77b0
Leonidas Lundell <leonidas.lundell@gmail.com>
2021-03-01 16:57:52 +0100

    changed clustering method

--- R/clustering_functions.R
+++ R/clustering_functions.R
@@ -17,7 +17,7 @@ clustereR <- function(ontoNet, ontoNames, ontoLength, target){
   connectedSubgraph <- shortest_paths(ontoNet, from = target, to = target, mode = "all")
   connectedSubgraph <- unique(names(unlist(connectedSubgraph$vpath)))
   ontoNetSubgraph <- igraph::induced_subgraph(ontoNet, connectedSubgraph)
-  ontoClust <- igraph::cluster_fast_greedy(as.undirected(ontoNetSubgraph))
+  ontoClust <- igraph::cluster_edge_betweenness(as.undirected(ontoNetSubgraph))
 
   #############
   #eigen centrality quantifies connected connecteness...
@@ -28,12 +28,40 @@ clustereR <- function(ontoNet, ontoNames, ontoLength, target){
   })
   names(clusterTerm) <- 1:max(ontoClust$membership)
 
+  #############
+  #prepare result table
+
   ontoClust <- data.frame(cluster = ontoClust$membership,
                           clusterTerm = clusterTerm[membership(ontoClust)],
                           ontoID = ontoClust$names,
                           ontoTerm = GOnames[ontoClust$names],
                           ontoLength = ontoLength[ontoClust$names])
 
-  ontoClust[ontoClust$ontoID %in% target,]
+  #############
+  #prepare network plot
+
+  cols <- colorRampPalette(brewer.pal(12, "Set3"))(max(ontoClust$cluster))
+  cols <- sample(cols, max(ontoClust$cluster), replace = F)
+  ontoClust$color <- cols[ontoClust$cluster]
+
+  V(ontoNetSubgraph)$color <- "black"
+  V(ontoNetSubgraph)$color[match(ontoClust$ontoID, V(ontoNetSubgraph)$name)] <- cols[ontoClust$cluster]
+  V(ontoNetSubgraph)$color[match(ontoClust$ontoID, V(ontoNetSubgraph)$name)] <- cols[ontoClust$cluster]
+  #V(ontoNetSubgraph)$name == ontoClust$ontoID
+  V(ontoNetSubgraph)$clusterTerm <- ontoClust$clusterTerm
+
+  V(ontoNetSubgraph)$size <- 0
+  V(ontoNetSubgraph)$size[V(ontoNetSubgraph)$name %in% x] <- 3
+
+  E(ontoNetSubgraph)$arrow.size <- 0
+
+
+  ontoNetSubgraph
+
+  #remove the "stepping stones"
+  ontoClust <- ontoClust[ontoClust$ontoID %in% target,]
+
+  return(list(res = ontoClust, plot = ontoNetSubgraph))
+
 }
 
