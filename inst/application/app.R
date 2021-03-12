3837ce13a548ebf56d466e0796710baf0e730ad9
Leonidas Lundell <leonidas.lundell@gmail.com>
2021-03-12 16:33:00 +0100

    modified the clustering page

--- inst/application/app.R
+++ inst/application/app.R
@@ -2,7 +2,7 @@ ui <- shinyUI(fluidPage(
   # Application title
   shiny::navbarPage("Ontology Descent: A data visualization tool for Gene Ontology Enrichment data",
                     shiny::tabPanel("Data Entry", data_entry_page_ui("data_entry")),
-                    shiny::tabPanel("Clustering", clustering_page_ui("clustering")),
+                    shiny::tabPanel("Clustering", exploring_page_ui("clustering")),
                     shiny::tabPanel("Sorting", sorting_page_ui("sorting")),
                     shiny::tabPanel("Plotting", plotting_page_ui("plotting")),
                     id = "active_page"
@@ -18,7 +18,7 @@ server <- shinyServer(function(input, output, session){
   })
 
   shiny::callModule(data_entry_page, "data_entry", descent_data)
-  shiny::callModule(clustering_page, "clustering", descent_data)
+  shiny::callModule(exploring_page, "clustering", descent_data)
   shiny::callModule(sorting_page, "sorting", descent_data)
   shiny::callModule(plotting_page, "plotting", descent_data)
 })
