a2ed63dc8aade532a1149b150e9a316aa31661c4
Lars Roed Ingerslev <ingerslev@sund.ku.dk>
2021-03-12 14:24:12 +0100

    Revert "reset app page"
    This reverts commit 1a49d91d9e0b40eb8a1f80ced031ec6269ef8bd7.

--- descent_app/app.R
+++ descent_app/app.R
@@ -1,18 +1,104 @@
-ui <- shinyUI(fluidPage(
-  # Application title
-  shiny::navbarPage("Ontology Descent: A data visualization tool for Gene Ontology Enrichment data",
-                    shiny::tabPanel("Data Entry", data_entry_page_ui("data_entry")),
-                    shiny::tabPanel("Clustering", clustering_page_ui("clustering")),
-                    shiny::tabPanel("Sorting", sorting_page_ui("sorting")),
-                    shiny::tabPanel("Plotting", plotting_page_ui("plotting"))
+library(shiny)
+library(DT)
+library(tidyverse)
+
+ui <- fluidPage(
+  titlePanel(
+    "Ontology Descent: A data visualization tool for Gene Ontology Enrichment data"
+  ),
+  tabsetPanel(type = "tabs",
+              tabPanel("Data Entry",
+  fluidRow(
+    column(
+      2,
+      textAreaInput(
+        "Terms",
+        h4("Ontology Terms"),
+        value = "Enter a list of ontology terms (ex. GO:0051004)",
+        height = "100%",
+        rows = 10,
+        resize = "both"
+      ),
+      actionButton("Setting1", label = "Submit!")
+    ),
+    column(
+      2,
+      textAreaInput(
+        "Enrichment",
+        h4("Enrichment Scores"),
+        value = "Enrichment Scores",
+        height = "100%",
+        rows = 10,
+        resize = "both"
+      )
+    ),
+    column(
+      2,
+      textAreaInput(
+        "pValues",
+        h4("Point Size"),
+        value = "Enter matching P-values",
+        height = "100%",
+        rows = 10,
+        resize = "both"
+      )
+    ),
+    column(
+      2,
+           textAreaInput(
+             "Direction",
+             h4("Enrichment Direction"),
+             value = "Enrichment direction (up or down)",
+             height = "100%",
+             rows = 10,
+             resize = "both")
+  ),
+  column(2, selectInput(
+    "dummy",
+    label = "Enable P-hacking?",
+    choices = c("Yes!", "Hell no!")
+  ),selectInput(
+    "dummy_2",
+    label = "Bless with Leo's beard??",
+    choices = c("Yes!", "Hell no!")
+  ),
+  selectInput(
+    "dummy_3",
+    label = "Do you want a banana?",
+    choices = c("Yes!", "Hell no!")
   )
-))
+  )
+  ),
+  dataTableOutput(outputId = "GO_table")),
+  tabPanel("Data Trimming", ""),
+  tabPanel("Data Visualization", "")))
 
-server <- shinyServer(function(input, output, session){
-  shiny::callModule(data_entry_page, "data_entry", descent_data)
-  shiny::callModule(clustering_page, "clustering", descent_data)
-  shiny::callModule(sorting_page, "sorting", descent_data)
-  shiny::callModule(plotting_page, "plotting", descent_data)
-})
 
-shiny::shinyApp(ui, server)
+
+
+server <- function(input,output){
+  data <- reactiveValues(InputData = data.frame(
+    GO_terms = NA,
+    Enrichment = NA,
+    pValues = NA,
+    Direction = NA
+  ))
+  #change pValue to Leo's stupid annotation
+
+     observeEvent(input$Setting1, {
+       data_matrix<- data.frame(unlist(strsplit(input$Terms, "\n")),
+                                unlist(strsplit(input$Enrichment, "\n")),
+                                unlist(strsplit(input$pValues, "\n")),
+                                unlist(strsplit(input$Direction, "\n")))
+       colnames(data_matrix) <- c("GO Terms", "Enrichment", "P-values", "Direction")
+       data$InputData<- data_matrix
+       output$GO_table <- renderDataTable(data$InputData)
+       #}
+       #add function that returns error if uneven length of our four factors are returned
+       })
+
+
+}
+
+
+shinyApp(ui = ui, server = server)
