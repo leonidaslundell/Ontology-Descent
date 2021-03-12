af9bfd77f971ab4411a9643c54d6c0224a32ab84
Lars Roed Ingerslev <ingerslev@sund.ku.dk>
2021-03-12 14:22:16 +0100

    Revert "page to manually sort clusters"
    This reverts commit 7c1b12474335ad95adbc22989b1df70e81d02722.

--- inst/application/app.R
+++ inst/application/app.R
@@ -3,23 +3,17 @@ ui <- shinyUI(fluidPage(
   shiny::navbarPage("Ontology Descent: A data visualization tool for Gene Ontology Enrichment data",
                     shiny::tabPanel("Data Entry", data_entry_page_ui("data_entry")),
                     shiny::tabPanel("Clustering", clustering_page_ui("clustering")),
-                    shiny::tabPanel("Sorting", sorting_page_ui("sorting")),
                     shiny::tabPanel("Plotting", plotting_page_ui("plotting")),
-                    id = "active_page"
+                    id = "mainApp"
   )
 ))
 
-server <- shinyServer(function(input, output, session){
+server <- shinyServer(function(input, output){
 
   descent_data <- get_test_data()
 
-  observe({
-    descent_data$active_page <- input$active_page
-  })
-
   shiny::callModule(data_entry_page, "data_entry", descent_data)
   shiny::callModule(clustering_page, "clustering", descent_data)
-  shiny::callModule(sorting_page, "sorting", descent_data)
   shiny::callModule(plotting_page, "plotting", descent_data)
 })
 
