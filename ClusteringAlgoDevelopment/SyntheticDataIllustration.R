86fafb4e0dd0f19af4bcac48aee20655b3ca0190
Leonidas Lundell <leonidas.lundell@gmail.com>
2021-03-12 14:10:04 +0100

    schematics for article

--- ClusteringAlgoDevelopment/SyntheticDataIllustration.R
+++ ClusteringAlgoDevelopment/SyntheticDataIllustration.R
@@ -1,7 +1,7 @@
 library(dendextend)
 library(magrittr)
 
-pdf("ClusteringAlgoDevelopment/figures/schematic.pdf", height = 5, width = 5 )
+pdf("ClusteringAlgoDevelopment/figures/schematic.benchmark.pdf", height = 5, width = 5 )
 
 set.seed(42)
 g <- make_tree(20, 8)
@@ -37,7 +37,7 @@ dend %>% set("branches_k_color",c("gray", "red"), k = 2) %>%  set("labels", NULL
 
 dend %>% set("branches_k_color",c("gray", "red"), k = 2) %>%  set("labels", NULL) %>%
   set("leaves_pch", 19) %>%  # node point type
-  set("leaves_cex", 1) %>%
+  set("leaves_cex", 1.5) %>%
   set("leaves_col", c("NA","blue")[dendCut]) %>%
   plot(axes = F, main = "Sample branch: \n Get a proportion of leaves")
 
@@ -105,42 +105,93 @@ plot(clusters[,-3] * abs(rnorm(38, mean = 1, sd = 0.04)),
 
 dev.off()
 
-pdf("ClusteringAlgoDevelopment/figures/accuracyRecallPression.pdf")
+pdf("ClusteringAlgoDevelopment/figures/schematic.walktrap.pdf", height = 5, width = 5 )
 
+y <- data.frame(from = c(2,3,4,5,11,21,31), to = c(1,1,1,1, 12,22,32))
+x <- graph_from_data_frame(y, directed = F)
+V(x)$arrow.size <- 0
+V(x)$label <- NA
+V(x)$color <- "gray"
+V(x)$size <- 12
+
+V(x)$color[nchar(V(x)$name) == 1] <- "purple"
+V(x)$color[V(x)$name %in% c(11,12)] <- "blue"
+V(x)$color[V(x)$name %in% c(21,22)] <- "darkgreen"
+V(x)$color[V(x)$name %in% c(31,32)] <- "darkred"
+
+plot(x, vertex.color = "gray")
+plot(x)
+
+yy <- rbind(y, data.frame(from = c(12,22,32, 1111, 1112, 1113,1113),
+                          to = c(999,999,999, 999, 1111, 1112,1)))
+xx <- graph_from_data_frame(yy, directed = F)
+V(xx)$arrow.size <- 0
+V(xx)$label <- NA
+V(xx)$color <- "gray"
+
+V(xx)$shape <- "circle"
+V(xx)$size <- 12
+
+V(xx)$shape[nchar(V(xx)$name) >= 3]  <- "square"
+# V(xx)$size[nchar(V(xx)$name) >= 3]  <- 5
+V(xx)$color[nchar(V(xx)$name) >= 3] <- "black"
+
+V(xx)$color[nchar(V(xx)$name) == 1] <- "purple"
+V(xx)$color[nchar(V(xx)$name) == 2] <- "lightblue"
 set.seed(42)
-clusters <- temp <- data.frame(x = c(rep(4:5, 2),
-                                     rep(5:7, 2),
-                                     rep(1:3, 3)),
-                               y = c(sort(rep(1:2, 2)),
-                                     sort(rep(4:6, 2)),
-                                     sort(rep(4:6, 3))),
-                               col = c(rep("purple", 4),
-                                       rep("orange", 6),
-                                       rep("blue", 9)))
+plot(xx)
 
-plot(clusters[,-3] * abs(rnorm(38, mean = 1, sd = 0.04)),
-     main = "Specificity - TPR\nTrue positive/(true positive + false positive)\n ie. trust when yes",
-     sub = "TPR=1/(1+1)=0.5",
-     col = clusters$col,
-     pch = c(rep(17,10), rep(15,9)),
-     cex = 3,
-     axes = F,
-     xlab = "", ylab = "")
-legend(0.7, 2.5, c("Predicted cluster 1", "Predicted cluster 2",
-                   c("True cluster A", "True cluster B", "True clustert C")),
-       pch = c(17, 15, 16,16, 16),
-       cex = 1, bty = "n", col = c("black", "black", "purple", "orange", "blue"))
+V(xx)$color[V(xx)$name %in%  c(1:5)] <- "purple"
+V(xx)$color[V(xx)$name %in% c(11,12)] <- "blue"
+V(xx)$color[V(xx)$name %in% c(21,22)] <- "darkgreen"
+V(xx)$color[V(xx)$name %in% c(31,32)] <- "darkred"
+set.seed(42)
+plot(xx)
 
-plot(clusters[,-3] * abs(rnorm(38, mean = 1, sd = 0.04)),
-     main = "Specificity - TPR\nTrue positive/(true positive + false positive)\n ie. trust when yes",
-     sub = "TPR=1/(1+1)=0.5",
-     col = clusters$col,
-     pch = c(rep(17,10), rep(15,9)),
-     cex = 3,
-     axes = F,
-     xlab = "", ylab = "")
-legend(0.7, 2.5, c("Predicted cluster 1", "Predicted cluster 2",
-                   c("True cluster A", "True cluster B", "True clustert C")),
-       pch = c(17, 15, 16,16, 16),
-       cex = 1, bty = "n", col = c("black", "black", "purple", "orange", "blue"))
+V(xx)$color[nchar(V(xx)$name) < 4] <- "gray"
+set.seed(42)
+plot(xx)
+
+plot.new()
+legend(0,0.5, legend = c("Significant ontology term", "Known ontology term"), pch = 16:15, cex = 1.6, col = c("gray", "black"))
 dev.off()
+
+# pdf("ClusteringAlgoDevelopment/figures/accuracyRecallPression.pdf")
+#
+# set.seed(42)
+# clusters <- temp <- data.frame(x = c(rep(4:5, 2),
+#                                      rep(5:7, 2),
+#                                      rep(1:3, 3)),
+#                                y = c(sort(rep(1:2, 2)),
+#                                      sort(rep(4:6, 2)),
+#                                      sort(rep(4:6, 3))),
+#                                col = c(rep("purple", 4),
+#                                        rep("orange", 6),
+#                                        rep("blue", 9)))
+#
+# plot(clusters[,-3] * abs(rnorm(38, mean = 1, sd = 0.04)),
+#      main = "Specificity - TPR\nTrue positive/(true positive + false positive)\n ie. trust when yes",
+#      sub = "TPR=1/(1+1)=0.5",
+#      col = clusters$col,
+#      pch = c(rep(17,10), rep(15,9)),
+#      cex = 3,
+#      axes = F,
+#      xlab = "", ylab = "")
+# legend(0.7, 2.5, c("Predicted cluster 1", "Predicted cluster 2",
+#                    c("True cluster A", "True cluster B", "True clustert C")),
+#        pch = c(17, 15, 16,16, 16),
+#        cex = 1, bty = "n", col = c("black", "black", "purple", "orange", "blue"))
+#
+# plot(clusters[,-3] * abs(rnorm(38, mean = 1, sd = 0.04)),
+#      main = "Specificity - TPR\nTrue positive/(true positive + false positive)\n ie. trust when yes",
+#      sub = "TPR=1/(1+1)=0.5",
+#      col = clusters$col,
+#      pch = c(rep(17,10), rep(15,9)),
+#      cex = 3,
+#      axes = F,
+#      xlab = "", ylab = "")
+# legend(0.7, 2.5, c("Predicted cluster 1", "Predicted cluster 2",
+#                    c("True cluster A", "True cluster B", "True clustert C")),
+#        pch = c(17, 15, 16,16, 16),
+#        cex = 1, bty = "n", col = c("black", "black", "purple", "orange", "blue"))
+# dev.off()
