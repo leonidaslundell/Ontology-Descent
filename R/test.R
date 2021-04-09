fee0a6a71f9f1f3bb0222aef580861282bab223d
Leonidas Lundell <leonidas.lundell@gmail.com>
2021-04-09 14:55:15 +0200

    changed dummy data function to output both allot and little dummy date

--- R/test.R
+++ R/test.R
@@ -1,14 +1,32 @@
-get_test_data <- function() {
-  example_data <-
-    paste(
-      example_data$ontoID,
-      example_data$pValue,
-      example_data$enrichmentScore,
-      example_data$direction,
-      sep = "\t",
-      collapse = "\n"
-    )
-  example_data <- paste0("ontoID\tpValue\tenrichmentScore\tdirection\n", example_data)
+get_test_data <- function(size = "big") {
+  if(size == "big"){
+    example_data <-
+      paste(
+        example_data$ontoID,
+        example_data$pValue,
+        example_data$enrichmentScore,
+        example_data$direction,
+        sep = "\t",
+        collapse = "\n"
+      )
+    example_data <- paste0("ontoID\tpValue\tenrichmentScore\tdirection\n", example_data)
 
-  example_data
+    return(example_data)
+  }else{
+    set.seed(42)
+    rand <- sample(1:length(nrow(example_data)), 50)
+    example_data <-
+      paste(
+        example_data$ontoID[rand],
+        example_data$pValue[rand],
+        example_data$enrichmentScore[rand],
+        example_data$direction[rand],
+        sep = "\t",
+        collapse = "\n"
+      )
+    example_data <- paste0("ontoID\tpValue\tenrichmentScore\tdirection\n", example_data)
+
+    return(example_data)
+  }
+
 }
