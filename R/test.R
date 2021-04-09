a7b72cf431d414c740f499af3ea3c45fb9e9dc2c
Morten Dall <mortendall1988@gmail.com>
2021-04-09 15:14:50 +0200

    Removed head merge

--- R/test.R
+++ R/test.R
@@ -14,11 +14,7 @@ get_test_data <- function(size = "big") {
     return(example_data)
   }else{
     set.seed(42)
-<<<<<<< HEAD
     rand <- sample(1:nrow(example_data), 50)
-=======
-    rand <- sample(1:length(nrow(example_data)), 50)
->>>>>>> fee0a6a71f9f1f3bb0222aef580861282bab223d
     example_data <-
       paste(
         example_data$ontoID[rand],
