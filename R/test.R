bd1be7c453ecd49ab34b1f3b851caef74265057a
Leonidas Lundell <leonidas.lundell@gmail.com>
2021-04-12 09:53:17 +0200

    changed the input GO terms to originate from GSE15471 and also be random sample of  either 300 or 50 terms from 800 terms to better show the capabilites of ontodesc

--- R/test.R
+++ R/test.R
@@ -1,11 +1,12 @@
 get_test_data <- function(size = "big") {
   if(size == "big"){
+    rand <- sample(1:nrow(example_data), nrow(example_data), replace = F)
     example_data <-
       paste(
-        example_data$ontoID,
-        example_data$pValue,
-        example_data$enrichmentScore,
-        example_data$direction,
+        example_data$ontoID[rand],
+        example_data$pValue[rand],
+        example_data$enrichmentScore[rand],
+        example_data$direction[rand],
         sep = "\t",
         collapse = "\n"
       )
@@ -13,8 +14,7 @@ get_test_data <- function(size = "big") {
 
     return(example_data)
   }else{
-    set.seed(42)
-    rand <- sample(1:nrow(example_data), 50)
+    rand <- sample(1:nrow(example_data), 50, replace = F)
     example_data <-
       paste(
         example_data$ontoID[rand],
