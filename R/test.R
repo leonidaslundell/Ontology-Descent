449b17f4e83af2d213d8c68f09a33eca507fa887
Lars Roed Ingerslev <ingerslev@sund.ku.dk>
2021-04-16 14:37:39 +0200

    fix test data

--- R/test.R
+++ R/test.R
@@ -1,32 +1,21 @@
 get_test_data <- function(size = "big") {
+
   if(size == "big"){
-    rand <- sample(1:nrow(example_data), nrow(example_data), replace = F)
-    example_data <-
-      paste(
-        example_data$ontoID[rand],
-        example_data$pValue[rand],
-        example_data$enrichmentScore[rand],
-        example_data$direction[rand],
-        sep = "\t",
-        collapse = "\n"
-      )
-    example_data <- paste0("ontoID\tpValue\tenrichmentScore\tdirection\n", example_data)
-
-    return(example_data)
-  }else{
+    rand <- sample(1:nrow(example_data), 300, replace = F)
+  } else {
     rand <- sample(1:nrow(example_data), 50, replace = F)
-    example_data <-
-      paste(
-        example_data$ontoID[rand],
-        example_data$pValue[rand],
-        example_data$enrichmentScore[rand],
-        example_data$direction[rand],
-        sep = "\t",
-        collapse = "\n"
-      )
-    example_data <- paste0("ontoID\tpValue\tenrichmentScore\tdirection\n", example_data)
-
-    return(example_data)
   }
 
+  example_data <-
+    paste(
+      example_data$ontoID[rand],
+      example_data$pValue[rand],
+      example_data$enrichmentScore[rand],
+      example_data$direction[rand],
+      sep = "\t",
+      collapse = "\n"
+    )
+  example_data <- paste0("ontoID\tpValue\tenrichmentScore\tdirection\n", example_data)
+
+  return(example_data)
 }
