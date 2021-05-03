219c230fd9422a84a44418f93df09088e00eb537
Morten Dall <mortendall1988@gmail.com>
2021-05-03 14:55:54 +0200

    #'ed the sorting page

--- R/app_server.R
+++ R/app_server.R
@@ -1,14 +1,14 @@
 #' The application server-side
-#' 
-#' @param input,output,session Internal parameters for {shiny}. 
+#'
+#' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
 app_server <- function( input, output, session ) {
   descent_data <- reactiveValues()
-  
+
   shiny::callModule(data_entry_page, "data_entry", descent_data)
   shiny::callModule(exploring_page, "clustering", descent_data)
-  shiny::callModule(sorting_page, "sorting", descent_data)
+  #shiny::callModule(sorting_page, "sorting", descent_data)
   shiny::callModule(plotting_page, "plotting", descent_data)
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -1,6 +1,6 @@
 #' The application User-Interface
-#' 
-#' @param request Internal parameter for `{shiny}`. 
+#'
+#' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
 #' @import shiny
 #' @noRd
@@ -10,26 +10,26 @@ app_ui <- function(request) {
     shiny::navbarPage(title = "Ontology Descent: summarizing and visualizing complex enrichment data",
                       shiny::tabPanel("Data Entry", data_entry_page_ui("data_entry")),
                       shiny::tabPanel("Clustering", exploring_page_ui("clustering")),
-                      shiny::tabPanel("Sorting", sorting_page_ui("sorting")),
+                     # shiny::tabPanel("Sorting", sorting_page_ui("sorting")),
                       shiny::tabPanel("Plotting", plotting_page_ui("plotting")),
                       id = "mainApp")
   )
 }
 
 #' Add external Resources to the Application
-#' 
-#' This function is internally used to add external 
-#' resources inside the Shiny application. 
-#' 
+#'
+#' This function is internally used to add external
+#' resources inside the Shiny application.
+#'
 #' @import shiny
 #' @importFrom golem add_resource_path activate_js favicon bundle_resources
 #' @noRd
 golem_add_external_resources <- function(){
-  
+
   add_resource_path(
     'www', app_sys('app/www')
   )
- 
+
   tags$head(
     favicon(),
     bundle_resources(
@@ -37,7 +37,7 @@ golem_add_external_resources <- function(){
       app_title = 'OntologyDescent'
     )
     # Add here other external resources
-    # for example, you can add shinyalert::useShinyalert() 
+    # for example, you can add shinyalert::useShinyalert()
   )
 }
 
