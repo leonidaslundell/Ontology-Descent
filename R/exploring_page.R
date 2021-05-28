f6fb73a0ee286e886c3735c4edcc2532fd4aef20
Morten Dall <mortendall1988@gmail.com>
2021-05-28 11:11:12 +0200

    fixed the hover bug

--- R/exploring_page.R
+++ R/exploring_page.R
@@ -176,19 +176,13 @@ exploring_page <- function(input, output, session, descent_data) {
       if (nrow(res) == 0) {
         return()
       }
-      # calculate point position INSIDE the image as percent of total dimensions
-      # from left (horizontal) and from top (vertical)
+
       hover <- input$netHover
-      left_pct <- (hover$x - hover$domain$left) / (hover$domain$right - hover$domain$left)
-      top_pct <- (hover$domain$top - hover$y) / (hover$domain$top - hover$domain$bottom)
-
-      # calculate distance from left and bottom side of the picture in pixels
-      left_px <- hover$range$left + left_pct * (hover$range$right - hover$range$left)
-      top_px <- hover$range$top + top_pct * (hover$range$bottom - hover$range$top)
-
+      left_px <- hover$coords_css$x
+      top_px <- hover$coords_css$y
       style <- paste0(
         "position:absolute; z-index:100; background-color: rgba(245, 245, 245, 0.85); ",
-        "left:", left_px+5, "px; top:", top_px+5, "px;"
+        "left:", left_px, "px; top:", top_px, "px;"
       )
 
       wellPanel(
